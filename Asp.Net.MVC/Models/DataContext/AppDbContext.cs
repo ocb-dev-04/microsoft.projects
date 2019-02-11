82620526d1e2fafbfd3a9e21b8a04dcdd074e2e2
ocb-dev-04 <42700726+ocb-dev-04@users.noreply.github.com>
2019-02-11 14:43:35 -0400

    *Add basic codes and nugets

---
+++ Asp.Net.MVC/Models/DataContext/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Asp.Net.MVC.Models.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        #region Construct
+
+        public AppDbContext()
+            :base("DefaultConnection")
+        {
+
+        }
+
+        #endregion
+
+        #region DbSet's
+
+        public DbSet<BlogPost> BlogPost { get; set; }
+
+        #endregion
+    }
+}
