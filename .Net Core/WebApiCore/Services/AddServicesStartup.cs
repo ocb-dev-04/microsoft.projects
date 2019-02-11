c44085e36bcd472269c29805a7b8c757aa7c4e1d
ocb-dev-04 <42700726+ocb-dev-04@users.noreply.github.com>
2019-02-11 15:25:25 -0400

    Add .net core project

---
+++ .Net Core/WebApiCore/Services/AddServicesStartup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ModelCore.Data;
+using ModelCore.Interfaces;
+using ModelCore.Repositories;
+
+namespace WebApiCore.Services
+{
+    public static class AddServicesStartup
+    {
+
+        public static void AddAppRandI(this IServiceCollection services)
+        {
+            services.AddScoped<
+                ICategoriesRepository,
+                CategoriesRepository>();
+
+            services.AddScoped<
+                IProductsRepository,
+                ProductsRepository>();
+        }
+
+        public static void AddDBInfo(this IServiceCollection services, IConfiguration Configuration)
+        {/*
+            services.AddDbContext<AppContext>(
+                optrions => optrions.UseInMemoryDatabase("ApiCore_ExampleDB"));
+        */
+            var connection = @"Server=.;Database=API_Core_TestDB;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<AppContext>
+                (options => options.UseSqlServer(connection));
+
+        }
+
+    }
+}
---
+++ .Net Core/WebApiCore/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ModelCore.Data;
+using ModelCore.Entities;
+using WebApiCore.Services;
+
+namespace WebApiCore
+{
+    public class Startup
+    {
+        #region Properties
+
+        public IConfiguration Configuration { get; }
+
+        #endregion
+
+        #region Construct
+
+        public Startup(IConfiguration configuration) 
+            => Configuration = configuration;
+
+        #endregion
+
+        #region ConfigureServices
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAppRandI();
+            services.AddDBInfo(Configuration);
+
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(ConfigureJson);
+        }
+
+        private void ConfigureJson(MvcJsonOptions obj) 
+            => obj.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+        #endregion
+
+        #region Configure ( app/env )
+
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env,
+            AppContext appContext)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            
+
+        }
+
+        #endregion
+    }
+}
